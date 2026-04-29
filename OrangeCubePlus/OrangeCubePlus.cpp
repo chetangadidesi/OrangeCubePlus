8f141a0bca3a3ecb9bf9509e7e01892459103192
chetangadidesi <chetan.gadidesi@gmail.com>
2026-04-29 17:28:55 -0500

    created github repo

---
+++ OrangeCubePlus/OrangeCubePlus.cpp
@@ -0,0 +1,12 @@
+﻿// OrangeCubePlus.cpp : Defines the entry point for the application.
+//
+
+#include "OrangeCubePlus.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
